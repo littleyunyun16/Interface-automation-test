fc2b943fd0141b1d3f02fa31d6549ff63a80f59d
littleyunyun16 <1563115157@qq.com>
2020-12-02 22:44:03 +0800

    优化

--- testcase/test01case.py
+++ testcase/test01case.py
@@ -2,31 +2,28 @@
 
 
 import testfile.readExcel as readexcel
-# import paramunittest
+import paramunittest
 import unittest
 import json
 import urllib.parse
 from common.configHttp import RunMain
-import ddt
+
 
 return_xls = readexcel.readExcel().get_xls('ReturnOrder.xlsx', 'Sheet1')  # 由文件readExcel来获取用例
-print(return_xls)
 
-
-@ddt.ddt
+@paramunittest.parametrized(*return_xls)
 class testUserLogin(unittest.TestCase):
-    @ddt.data(*return_xls)
-    def setParameters(self,data):
+    def setParameters(self,id,case_name,url,method,params,expect,response,result,passed):
 
-        self.id = data['id']
-        self.case_name = data['case_name']
-        self.url = data['url']
-        self.method = data['method']
-        self.params = data['params']
-        self.expect=data['expect']
-        self.response=data['response']
-        self.result=data['result']
-        self.passed=data['passed']
+        self.id = id
+        self.case_name = case_name
+        self.url = url
+        self.method = method
+        self.params = params
+        self.expect=expect
+        self.response=response
+        self.result=result
+        self.passed=passed
 
     def setUp(self):
         print(self.id+ "测试开始前准备")
@@ -37,6 +34,8 @@ class testUserLogin(unittest.TestCase):
         params = eval(self.params)
         info = RunMain().run_main(self.method, self.url, params)
         ss = json.loads(info)
+        self.assertTrue('success')
+
 
 
 
